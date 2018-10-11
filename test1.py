5868ba95cd412fa592f22bfe82144a460d693e67
lijl <longliljl@126.com>
2018-10-11 11:33:25 +0800

    Initial commit

---
+++ test1.py
@@ -0,0 +1,17 @@
+import sys
+print('================Python import mode==========================');
+print ('命令行参数为:')
+for i in sys.argv:
+    print (i)
+print ('\n python 路径为',sys.path)
+x="a"
+y="b"
+# 换行输出
+print( x )
+print( y )
+ 
+print('---------')
+# 不换行输出
+print( x, end=" " )
+print( y, end=" " )
+print()
